1061569f826f21801164876d6bd64b0a34320cb9
Lucas Mendes <lucaslsbh1@gmail.com>
2023-02-12 20:52:00 -0300

    Add main.go containing the application bootstrapping

---
+++ cmd/grpc/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"context"
+
+	"github.com/lucasmls/dd/internal/orders/adapters/repositories"
+	resolvers "github.com/lucasmls/dd/internal/orders/ports/grpc"
+
+	"github.com/lucasmls/dd/pkg/grpc"
+	"github.com/lucasmls/dd/pkg/protog"
+	"go.uber.org/zap"
+	gGRPC "google.golang.org/grpc"
+)
+
+func main() {
+	ctx := context.Background()
+
+	logger, _ := zap.NewProduction()
+	defer logger.Sync()
+
+	orderRepository := repositories.MustNewInMemoryOrdersRepository(
+		logger,
+		10,
+	)
+
+	ordersResolver := resolvers.MustNewOrdersResolver(
+		logger,
+		orderRepository,
+	)
+
+	grpcServer := grpc.MustNewServer(
+		logger,
+		3001,
+		nil,
+		func(server gGRPC.ServiceRegistrar) {
+			protog.RegisterOrdersServiceServer(server, ordersResolver)
+		},
+	)
+
+	if err := grpcServer.Run(ctx); err != nil {
+		logger.Fatal("failed to run gRPC server", zap.Error(err))
+	}
+}
